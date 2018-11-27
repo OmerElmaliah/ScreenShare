2d68f3251ec249f63eea3ca15997c2220ca6c23a
OmerElmaliah <omerelmaliahoe@gmail.com>
2018-11-27 09:35:25 +0200

    Enables mouse control

--- customer.py
+++ customer.py
@@ -1,6 +1,9 @@
 """Customer Class - Sends current screen image"""
 import socket
 from PIL import ImageGrab, Image
+import pyautogui
+import pickle
+import time
 
 
 class Customer(object):
@@ -12,23 +15,33 @@ class Customer(object):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.socket.bind((self.ip_src, self.port_src))
         self.main_con = True
+        pyautogui.FAILSAFE = False
         # TODO: Add an exit button with exit_button_ui.py
 
-    def start_work(self):
+    def run(self):
         while self.main_con:
-            img = ImageGrab.grab()
-            img.thumbnail((1080, 720), Image.ANTIALIAS)
-            img.save('img.png')
+            self.start_work()
+            self.recv_mouse_click()
 
-            with open('img.png', 'rb') as screen_image:
+    def start_work(self):
+        img = ImageGrab.grab()
+        img.thumbnail((1080, 720), Image.ANTIALIAS)
+        img.save('img.png')
+
+        with open('img.png', 'rb') as screen_image:
+            img_data = screen_image.read(8192)
+            while img_data:
+                self.socket.sendto(img_data, (self.ip_dst, self.port_dst))
                 img_data = screen_image.read(8192)
-                while img_data:
-                    self.socket.sendto(img_data, (self.ip_dst, self.port_dst))
-                    img_data = screen_image.read(8192)
-                self.socket.sendto("Image sent!".encode('utf-8'), (self.ip_dst, self.port_dst))
-                screen_image.close()
+            self.socket.sendto("Image sent!".encode('utf-8'), (self.ip_dst, self.port_dst))
+            screen_image.close()
 
-        self.socket.close()
+    def recv_mouse_click(self):
+        mouse_input = pickle.loads(self.socket.recv(1024))
+        if (str(mouse_input) == "left") or (str(mouse_input) == "right"):
+            pyautogui.click(button=mouse_input)
+        else:
+            pyautogui.moveTo(mouse_input)
 
     def set_connection_status(self, con):
         self.main_con = con
@@ -36,5 +49,3 @@ class Customer(object):
     def close_connection(self):
         self.main_con = False
         self.socket.close()
-
-# TODO: Detect mouse/keyboard input, if none send screenshot
--- handler.py
+++ handler.py
@@ -3,6 +3,9 @@ import socket
 from PyQt5.QtGui import QPixmap
 from PyQt5 import QtWidgets
 import os.path
+import win32api
+import time
+import pickle
 
 
 class Handler(object):
@@ -17,30 +20,47 @@ class Handler(object):
         self.window.showMaximized()
         self.window.show()
 
-    def recv_img(self):
+    def run(self):
         while True:
-            rewrite_data = None
-            if os.path.isfile('D:\images\img.png'):
-                os.remove('D:\images\img.png')
-            img = open('D:\images\img.png', 'wb')
-            while True:
-                data = self.socket.recv(8192)
-                if rewrite_data is None:
-                    rewrite_data = data
-                else:
-                    rewrite_data = rewrite_data + data
-                if "Image sent!".encode('utf-8') in data:
-                    img.write(rewrite_data)
-                    img.close()
+            self.recv_img()
+            self.send_mouse_click()
 
-                    img_display = QPixmap('D:\images\img.png')
-                    self.window.setPixmap(img_display)
-                    self.window.setScaledContents(True)
-                    self.window.update()
-                    break
+    def recv_img(self):
+        rewrite_data = None
+        if os.path.isfile('D:\images\img.png'):
+            os.remove('D:\images\img.png')
+        img = open('D:\images\img.png', 'wb')
+        while True:
+            data = self.socket.recv(8192)
+            if rewrite_data is None:
+                rewrite_data = data
+            else:
+                rewrite_data = rewrite_data + data
+            if "Image sent!".encode('utf-8') in data:
+                img.write(rewrite_data)
+                img.close()
+
+                img_display = QPixmap('D:\images\img.png')
+                self.window.setPixmap(img_display)
+                self.window.setScaledContents(True)
+                self.window.update()
+                break
+
+    def send_mouse_click(self):
+        state_left = win32api.GetKeyState(0x01)
+        state_right = win32api.GetKeyState(0x02)
+        con = True
+
+        if state_left == -127 or state_left == -128:
+            self.socket.sendto(pickle.dumps("left"), (self.ip_dst, self.port_dst))
+            con = False
+
+        if (state_right == -127 or state_right == -128) and con:
+            self.socket.sendto(pickle.dumps("right"), (self.ip_dst, self.port_dst))
+            con = False
+
+        if con:
+            self.socket.sendto(pickle.dumps(win32api.GetCursorPos()), (self.ip_dst, self.port_dst))
 
     def close_connection(self):
         self.socket.close()
-
-
-# TODO: Create a filter function which will differentiate between mouseclicks/keyboard/recv_img commands
