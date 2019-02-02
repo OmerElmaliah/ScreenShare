3590fc600a2aa1fdc807391eaa8fdafeae33f6ec
Omer Elmaliah <34664238+OmerElmaliah@users.noreply.github.com>
2019-02-02 16:24:23 +0200

    restart v2

--- screenshare.py
+++ screenshare.py
@@ -1,12 +1,27 @@
 """Main Class - Initiates Components"""
 from PyQt5 import QtWidgets
 from App.main_window import MainWindow
+from App.login_window import LoginWindow
 import sys
 import multiprocessing
+import threading
 
 
-"""Initiates the app"""
+LOGIN_CONDITION = False
+
+
+def setup_login():
+    """Initiates the login page"""
+    global LOGIN_CONDITION
+    app = QtWidgets.QApplication(sys.argv)
+    login_window = LoginWindow()
+    login_window.show()
+    app.exec_()
+    LOGIN_CONDITION = login_window.logged_in
+
+
 def setup_main():
+    """Initiates the app"""
     app = QtWidgets.QApplication(sys.argv)
     main_window = MainWindow()
     main_window.show()
@@ -14,9 +29,14 @@ def setup_main():
 
 
 def main():
-    main_process = multiprocessing.Process(target=setup_main)
-    main_process.start()
-    main_process.join()
+    login_thread = threading.Thread(target=setup_login)
+    login_thread.start()
+    login_thread.join()
+
+    if LOGIN_CONDITION:
+        main_process = multiprocessing.Process(target=setup_main)
+        main_process.start()
+        main_process.join()
 
 
 if __name__ == '__main__':
